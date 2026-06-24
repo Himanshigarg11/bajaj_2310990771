559de34ead78420cb4c0e1ca1fac292b18b5998a
himanshi11 <himanshigarg1102@gmail.com>
2026-06-24 19:10:07 +0530

    fix render port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use("/bfhl", require("./routes/bfhl"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
